ba57f80c9eafaba0ead007b2e8acdc774cb534b0
pikapi <works.mustakim@gmail.com>
2025-11-29 00:03:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
 
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'operator@fbtms.com' && password === '12345678') {
+        // Simpan user session sederhana
+        localStorage.setItem('currentUser', 'Operator');
+        window.location.href = 'inspection.html';
+    } else {
+        alert('Invalid username or password. Please try again.');
+    }
+});
